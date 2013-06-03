d26a2e514e35d936edf18d2706295145cfd895bd
Bogdan Cernat <cernat.bogdan.stefan@gmail.com>
2013-06-04 00:35:37 +0300

    manual merge

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,67 @@
 var db = require('../db')
   , cr = require('crypto')
   , md5
+  , passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
   ;
-function getHash (str){
+
+exports.passport = passport;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new TwitterStrategy({
+    consumerKey: 's6wF0QuG4mRhwaZCWswA',
+    consumerSecret: 'TfLKblLHtYe6sAr5iOy2deSdbSdwQVYTjuGoBsgu9A',
+    callbackURL: "http://localhost:3000/loginTwitter/"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Twitter profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Twitter account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+passport.use(new GoogleStrategy({
+    clientID: '291727398085-2u4d917nj3cvdc1ltmonnkib8mrqp0j1.apps.googleusercontent.com',
+    clientSecret: 'ijuWf0OW6q_hIoedHAPSsphU',
+    callbackURL: 'http://localhost:3000/loginGoogle'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      console.log(profile);
+      return done(null, profile);
+    });
+  }
+));
+
+function getHash (user, callback){
   md5 = cr.createHash('md5');
-  return md5.update(str).digest('hex');
+  user['password'] = md5.update(user['password']).digest('hex');
+  callback(user);
 }
+
 exports.loginPage = function(req, res){
   res.render('login', {title: 'Login', msg: 'salut'});
 }
-exports.user = function(){
-  return userLogged;
-}
+
 exports.logout = function(req, res){
   res.clearCookie('todo_logged_in');
   res.redirect('/');
@@ -28,15 +78,16 @@ exports.login = function(req, res){
   db.getUser(user['email'], function (resp){
     if(resp){
       console.log(resp);
-      if(resp.value['password'] == getHash(user['password'])){
-        res.cookie("todo_logged_in",{
-          "user": user.email,
-          "_id": resp.value._id
-        },{
-          expires: new Date(Date.now()+99999999),
-          signed: true
-        });
-      }
+      getHash(user, function(user){
+        if (resp.value['password'] == user['password'])
+          res.cookie("todo_logged_in",{
+            "user": user.email,
+            "_id": req.sessionID
+          },{
+            expires: new Date(Date.now()+99999999),
+            signed: true
+          });
+      });
       res.redirect('/');  
     }
     else {
@@ -67,12 +118,25 @@ exports.register = function(req,res){
   user["email"] = user["email"].toLowerCase();
   if(allHaveValues){
     db.uniqueUser(user['email'],function (itIs){
-      if(itIs && user['password'] == user['repassword']){
+      if((itIs && user['password'] == user['repassword']) && (user['password'].length >= 6 && user['password'].length <= 32)){
         delete user['repassword'];
-        user['password'] = getHash(user['password']);
-        db.insert(user);
-        res.redirect('/');
-      }else{
+        var matches = user['email'].match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        if (matches != null && matches.length == 1) {
+          getHash(user, function(user) {
+            db.insert(user);
+            res.cookie("et_logged_in",{
+            "user": user['email'],
+            "_id": req.sessionID
+            },{
+              expires: new Date(Date.now()+99999999),
+              signed: true
+            });
+            res.redirect('/');
+          });
+        } else {
+          res.redirect('/');
+        }
+      } else {
         res.redirect('/');
       }
     });
