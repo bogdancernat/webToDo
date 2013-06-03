e3fbe62777a12981eaa238c1c3cfd21cbd288a96
Bogdan Cernat <cernat.bogdan.stefan@gmail.com>
2013-06-04 00:36:23 +0300

    manual merge - app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , auth = require('./routes/auth')
-  // , search = require('./routes/search')
+  , search = require('./routes/search')
   , path = require('path')
   , db = require('./db')
-  , io = require('socket.io').listen(server)
+  , io = require('socket.io').listen(server) 
   ;
    
 // all environments
@@ -28,6 +28,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('bestappevur'));
   app.use(express.session());
+  app.use(auth.passport.initialize());
+  app.use(auth.passport.session());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -44,11 +46,84 @@ app.get('/users', user.list);
 app.get('/login', auth.loginPage);
 app.get('/register', auth.registerPage);
 app.get('/logout',auth.logout);
-// app.get('/search',search.info);
+app.get('/search',search.info);
 app.post('/login', auth.login );
 app.post('/register', auth.register);
+app.get('/loginTwitter', function(req, res, next) {
+  auth.passport.authenticate('twitter', function(err, user, info) {
+    console.log(user);
+    if (user) {
+    res.cookie("et_logged_in",{
+            "user": user.displayName,
+            "_id": req.sessionID
+          },{
+            expires: new Date(Date.now()+99999999),
+            signed: true
+          });
+    }
+    res.redirect('/'); 
+  })(req, res, next);
+});
 
-io.sockets.on('connection',function(socket){
+app.get('/loginGoogle', function(req, res, next) {
+  auth.passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'] }, function(err, user, info) {
+    if (user) {
+      res.cookie("et_logged_in",{
+          "user": user.displayName,
+          "_id": req.sessionID
+        },{
+          expires: new Date(Date.now()+99999999),
+          signed: true
+        });
+    }
+    res.redirect('/'); 
+  })(req, res, next);
+});
+
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('addToDo', function (data){
+    console.log(data);
+    socket.emit('updateToDoList', data);
+  });
+
+  socket.on('validateEmail', function(data){
+      data.email = data.email.replace(/\s/g,'');
+      data.email = data.email.toLowerCase();
+      db.uniqueUser(data.email, function (itIs){
+        var matches = data.email.match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        if (matches == null || matches.length != 1) 
+          itIs = false;
+
+        data.isValid = itIs; 
+        socket.emit('validationResult', data);  
+      });
+  });
+
+  socket.on('validatePass', function(data){
+      var isValid = true;
+      if (data.password.length < 6 || data.password.length > 32)
+        isValid = false;
+
+      data.isValid = isValid;
+      socket.emit('validationResult', data);
+  });
+
+  socket.on('validateConfPass', function(data){
+    var isValid = true;
+
+    if (data.password != data.rePassword)
+      isValid = false;
+
+    data.isValid = isValid;
+
+    socket.emit('validationResult', data);
+  });
+  
   socket.on('addToDo', function(data){
     var item = data.data;
     var todo = {
@@ -66,6 +141,7 @@ io.sockets.on('connection',function(socket){
   });
 });
 
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
